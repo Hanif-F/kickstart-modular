a948894ff4e8210da0891fde32492a2ea5828341
Hans <haniffaris159@gmail.com>
2024-09-10 15:59:10 +0700

    Add some custom colorscheme

--- lua/kickstart/plugins/colorschemes.lua
+++ lua/kickstart/plugins/colorschemes.lua
@@ -31,8 +31,111 @@ return {
       }
 
       vim.opt.termguicolors = true
-      -- vim.cmd('colorscheme bluloco')
-      vim.cmd.colorscheme 'bluloco'
+    end,
+  },
+  {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('onedark').setup {
+        -- Main options --
+        style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = false, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        -- toggle theme style ---
+        toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none',
+        },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+        -- Custom Highlights --
+        colors = {}, -- Override default colors
+        highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true, -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      }
+    end,
+  },
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      require('cyberdream').setup {
+        -- Enable transparent background
+        transparent = false,
+
+        -- Enable italics comments
+        italic_comments = false,
+
+        -- Replace all fillchars with ' ' for the ultimate clean look
+        hide_fillchars = false,
+
+        -- Modern borderless telescope theme - also applies to fzf-lua
+        borderless_telescope = true,
+
+        -- Set terminal colors used in `:terminal`
+        terminal_colors = true,
+
+        -- Use caching to improve performance - WARNING: experimental feature - expect the unexpected!
+        -- Early testing shows a 60-70% improvement in startup time. YMMV. Disables dynamic light/dark theme switching.
+        cache = false, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+
+        theme = {
+          variant = 'default', -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+          highlights = {
+            -- Highlight groups to override, adding new groups is also possible
+            -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+            -- Example:
+            Comment = { fg = '#696969', bg = 'NONE', italic = true },
+
+            -- Complete list can be found in `lua/cyberdream/theme.lua`
+          },
+
+          -- Override a highlight group entirely using the color palette
+          overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+            -- Example:
+            return {
+              Comment = { fg = colors.green, bg = 'NONE', italic = true },
+              ['@property'] = { fg = colors.magenta, bold = true },
+            }
+          end,
+
+          -- Override a color entirely
+          colors = {
+            -- For a list of colors see `lua/cyberdream/colours.lua`
+            -- Example:
+            bg = '#000000',
+            green = '#00ff00',
+            magenta = '#ff00ff',
+          },
+        },
+
+        -- Disable or enable colorscheme extensions
+      }
     end,
   },
 }
