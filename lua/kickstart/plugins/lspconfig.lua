34f8345eda4d49e2d3b8d3b3a18abcbf3b5e5c11
Hans <haniffaris159@gmail.com>
2024-09-17 15:43:52 +0700

    Implement signature help in lspconfig
    Add config to activate signature help by using Ctrl-K while in insert mode to help with calling functions

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -108,6 +108,8 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          vim.api.nvim_buf_set_keymap(event.buf, 'i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
